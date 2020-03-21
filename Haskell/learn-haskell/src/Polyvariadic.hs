de482f34d162afe165bbfcda6e1debaff4d1d266
Michael Haynes <Antimike.Haynes@colostate.edu>
2020-03-21 11:51:50 -0600

    Added literate HS file on polyvariadic fns; added source file for practice implementations

---
+++ Haskell/learn-haskell/src/Polyvariadic.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving, UndecidableInstances, FlexibleContexts #-}
+
+module Polyvariadic where
+  import Control.Arrow
